eb4e6c863bd0f78193a206002ae79cf1213d5692
Tarun Mankar <53537979+tarunmankar@users.noreply.github.com>
2022-04-14 21:46:26 +0530

    Update arraysum.java

--- arrays/arraysum.java
+++ arrays/arraysum.java
@@ -1,13 +1,16 @@
-// Program to find sum of elements in a given array in java
+/* Program to find sum of elements in a given array in java
+Example - Input = 5,5,5,5,5
+          Output = 25
+*/
 
 public class ArrSum {
 
   public static void main(String[] args) {
-		int [] arr= {5,5,5,5,5};
-		int sum=0;
-        for(int i=0; i<arr.length; i++){
-        	sum=sum+arr[i];
-        }
-        System.out.print(sum);
+	int [] arr= {5,5,5,5,5};
+	int sum=0;
+		for(int i=0; i<arr.length; i++){
+			sum=sum+arr[i];
+		}
+          System.out.print(sum);
   	}
 }
