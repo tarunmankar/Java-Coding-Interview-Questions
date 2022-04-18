2bdd78d2208193f0611079b04a266c536cda5de1
Tarun Mankar <53537979+tarunmankar@users.noreply.github.com>
2022-04-18 15:53:59 +0530

    Create InsertionSort.java

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,15 @@
+class InsertionSort {
+   public static void main(String args[]) {
+       int arr[] = {7, 8, 1, 3, 2};
+       for(int i=1; i<arr.length; i++) {
+           int current = arr[i];
+           int j = i - 1;
+               while(j >= 0 && arr[j] > current) {
+                   arr[j+1] = arr[j];
+                   j--;
+               }
+           arr[j+1] = current;
+       }
+       System.out.println(Arrays.toString(arr));
+   }
+}
