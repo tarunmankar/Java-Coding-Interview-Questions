3179b03622f7e5edc3bcfa063b3face567047af8
Tarun Mankar <53537979+tarunmankar@users.noreply.github.com>
2022-04-18 15:44:55 +0530

    Create bubblesort.java

---
+++ sorting/bubblesort.java
@@ -0,0 +1,19 @@
+//bubble sort
+
+class Sorting {
+   public static void main(String[] args) {
+        int arr[] = {7, 8, 1, 3, 2};
+        for(int i=0; i<arr.length-1; i++) {
+           for(int j=0; j<arr.length-i-1; j++) {
+               if(arr[j] > arr[j+1]) {
+                   //swap
+                   int temp = arr[j];
+                   arr[j] = arr[j+1];
+                   arr[j+1] = temp;
+               }
+           }
+       } 
+      System.out.println(Arrays.toString(arr));
+   }
+}
+   
