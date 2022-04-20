40c106a015c606b77aa12be1e940c5fc56cb201d
Tarun Mankar <53537979+tarunmankar@users.noreply.github.com>
2022-04-21 01:12:30 +0530

    Update firstNonRepeating.java

--- arrays/firstNonRepeating.java
+++ arrays/firstNonRepeating.java
@@ -25,3 +25,36 @@ class Main {
         return -1;
     }
 }
+/* Time Complexity: O(n)
+   Auxiliary Space: O(n)
+_______________________________________________________________ 
+*/
+
+public class Main {
+    public static void main(String[] args) {
+       int arr[] = {1,4,7,2,6,8,1,4};
+       int n = arr.length;
+       System.out.print(firstNonRepeating(arr, n));
+    }
+    
+    static int firstNonRepeating(int arr[], int n)
+    {
+        Map<Integer, Integer> m = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (m.containsKey(arr[i])) {
+                m.put(arr[i], m.get(arr[i]) + 1);
+            }
+            else {
+                m.put(arr[i], 1);
+            }
+        }
+        
+        for (int i = 0; i < n; i++)
+            if (m.get(arr[i]) == 1)
+                return arr[i];
+        return -1;
+    }
+}
+
+// Time Complexity: O(n^2)
+//  Auxiliary Space: O(n^2)
