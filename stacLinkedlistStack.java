9ad9f303ef941960d01242c1e7b373296f89713b
Tarun Mankar <53537979+tarunmankar@users.noreply.github.com>
2022-07-14 21:12:14 +0530

    Stack using LinkedList

---
+++ stacLinkedlistStack.java
@@ -0,0 +1,73 @@
+package stack;
+
+public class LinkedlistStack {
+	    Node head;
+		class Node {
+	        int data;
+	        Node next;
+	
+	        Node(int data) {
+	            this.data = data;
+	            this.next = null;
+	        }
+	    }
+		
+//------------------------------------------------------------------------------------------------
+		
+		boolean isEmpty() {
+            return (head == null);
+        }
+
+//------------------------------------------------------------------------------------------------
+		
+        public void push(int data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+//------------------------------------------------------------------------------------------------
+
+        public void pop() {
+            if(isEmpty()) {
+            	System.out.print("Stack is Empty");
+            	return;
+            }
+            
+            head = head.next;
+        }
+
+//------------------------------------------------------------------------------------------------
+        
+        public void peek() {
+            if(isEmpty()) {
+            	System.out.print("Stack is Empty");
+            	return;
+            }
+            
+            System.out.print(head.data);
+        }
+
+        public void printList()
+        {
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+  //------------------------------------------------------------------------------------------------
+     
+//------------------------------------------------------------------------------------------------
+
+    public static void main(String args[]) {
+    	LinkedlistStack st = new LinkedlistStack();
+    	st.push(1);
+    	st.push(2);
+    	st.push(3);
+    	st.printList();
+        st.pop();
+        st.peek();
+        st.printList();
+    }
+}
